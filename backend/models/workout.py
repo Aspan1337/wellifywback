fae2207f286b7803c6627c0e22edceeb41e55075
Aspan1337 <taukenaspan07@gmail.com>
2025-05-02 15:31:26 +0500

    ghjk

--- backend/models/workout.py
+++ backend/models/workout.py
@@ -7,3 +7,9 @@ class Workout(db.Model):
     title = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    sections = db.relationship(
+        'WorkoutSection',
+        backref='workout',
+        cascade='all, delete-orphan',
+        passive_deletes=True
+    )
